3f1e932fabc1914cb160802f68f140b38c5ecfba
David Parr <dvdprr6@gmail.com>
2018-08-10 14:54:52 -0400

    finished provision

--- vagrant-setup-centos7/puppet/manifests/setup.pp
+++ vagrant-setup-centos7/puppet/manifests/setup.pp
@@ -48,12 +48,81 @@ class hadoop{
   }
 }
 
+class sshKeyGen{
+  exec{'generate rsa':
+    command => 'ssh-keygen -t rsa -P "" -f /home/vagrant/.ssh/id_rsa',
+    cwd => '/home/vagrant/.ssh',
+    creates => '/home/vagrant/.ssh/id_rsa',
+    user => vagrant
+  } ->
+  exec{'generate authorized keys':
+    command => 'cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys',
+    cwd => '/home/vagrant/.ssh',
+    user => vagrant
+  } ->
+  file{'/home/vagrant/.ssh/authorized_keys':
+    ensure => 'file',
+    owner => vagrant,
+    mode => '0600'
+  }
+}
+
+class timezone{
+  exec{'unlink localtime':
+    command => 'unlink /etc/localtime',
+    cwd => '/home/vagrant',
+    user => root
+  } ->
+  file{'/etc/localtime':
+    ensure => 'link',
+    target => '/usr/share/zoneinfo/Canada/Eastern',
+    owner => root
+  }
+}
+
+class env{
+  exec{'set environment variables':
+    command => 'cat /vagrant/env/envVariables >> /home/vagrant/.bashrc',
+    cwd => '/home/vagrant',
+    user => vagrant
+  }
+}
+
+class hadoopConfig{
+  file{'/home/vagrant/hadoop-2.7.7/etc/hadoop/core-site.xml':
+    ensure => 'file',
+    source => '/vagrant/conf/core-site.xml'
+  } ->
+    file{'/home/vagrant/hadoop-2.7.7/etc/hadoop/hdfs-site.xml':
+    ensure => 'file',
+    source => '/vagrant/conf/hdfs-site.xml'
+  } ->
+    file{'/home/vagrant/hadoop-2.7.7/etc/hadoop/mapred-site.xml':
+    ensure => 'file',
+    source => '/vagrant/conf/mapred-site.xml'
+  } ->
+    file{'/home/vagrant/hadoop-2.7.7/etc/hadoop/yarn-site.xml':
+    ensure => 'file',
+    source => '/vagrant/conf/yarn-site.xml'
+  }
+}
+
 class devSetup{
   include systemUpdate
   include java
+  include hadoop
+  include sshKeyGen
+  include timezone
+  include env
+  include hadoopConfig
   
   Class[systemUpdate]
   -> Class[java]
+  -> Class[hadoop]
+  -> Class[sshKeyGen]
+  -> Class[timezone]
+  -> Class[env]
+  -> Class[hadoopConfig]
 }
 
 include devSetup
